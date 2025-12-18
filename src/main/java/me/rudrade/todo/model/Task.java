e51c3197d5262e907955409edc0d9cc78b10adf8
Rui Andrade <andrade.rui99@gmail.com>
2025-12-18 01:04:21 +0000

    added unit tests

--- src/main/java/me/rudrade/todo/model/Task.java
+++ src/main/java/me/rudrade/todo/model/Task.java
@@ -9,10 +9,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Table(name = "task")
@@ -20,6 +17,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode
 public class Task {
 	
 	@Id
